7e8eeeb860e0518d903d410cda359768ce639c90
tytydraco <tylernij@gmail.com>
2020-10-04 12:55:38 -0700

    Do not use https protocol
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@ ui_print " * Fetching the latest module from GitHub..."
 for bin in charch cparch lsarch mkarch rmarch unarch
 do
 	ui_print " * Fetching $bin..."
-	wget -O "$MODPATH/system/bin/$bin" "https://raw.githubusercontent.com/tytydraco/ChArch/master/$bin"
+	wget -O "$MODPATH/system/bin/$bin" "http://raw.githubusercontent.com/tytydraco/ChArch/master/$bin"
 done
 
 ui_print " * Patching for use on Android..."
